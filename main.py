da48415c498aa022f0f47298bc037ec8cfc49b7c
jhawk7 <james.hawkins@careerbuilder.com>
2021-11-21 10:42:15 -0500

    updating main

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ async def kill():
 async def getStats():
 	stats = Stats()
 	stats.temp = runcmd("vcgencmd measure_temp").split("temp=")[1]
-	stats.cpu = runcmd("top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'")
-	stats.mem = runcmd("free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'")
-	stats.disk_space = runcmd("df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\'")
-	stats.ip = runcmd("hostname -I | cut -d' ' -f1")
+	stats.cpu = runcmd("top -bn1 | grep load | awk \'{printf \"CPU Load: %.2f\", $(NF-2)}\'")
+	stats.mem = runcmd("free -m | awk \'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }\'")
+	stats.disk_space = runcmd("df -h | awk \'$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}\'")
+	stats.ip = runcmd("hostname -I | cut -d\' \' -f1")
 
 
 def runcmd(cmd):
