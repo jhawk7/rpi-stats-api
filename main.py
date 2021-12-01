b3b87a16c16a144bed736a67cd0ae3a172b31a21
jhawk7 <james.hawkins@careerbuilder.com>
2021-12-01 18:17:10 -0500

    stats update

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ async def healthcheck():
 @app.get("/stats", response_model=Stats)
 async def getStats():
 	stats = Stats()
-	stats.temp = runcmd("vcgencmd measure_temp").split("temp=")[1]
+	#stats.temp = runcmd("vcgencmd measure_temp").split("temp=")[1]
+	stats.temp = "n/a"
 	stats.cpu = runcmd("top -bn1 | grep load | awk \'{printf \"CPU Load: %.2f\", $(NF-2)}\'")
 	stats.mem = runcmd("free -m | awk \'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }\'")
 	stats.disk_space = runcmd("df -h | awk \'$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}\'")
