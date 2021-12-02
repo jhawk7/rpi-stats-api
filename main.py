75bb29055b3961f336042254985fc6467d177907
jhawk7 <james.hawkins@careerbuilder.com>
2021-12-01 19:00:01 -0500

    fixing stats response

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ async def getStats():
 	stats = Stats()
 	#stats.temp = runcmd("vcgencmd measure_temp").split("temp=")[1]
 	stats.temp = "n/a"
-	stats.cpu = runcmd("top -bn1 | grep load | awk \'{printf \"CPU Load: %.2f\", $(NF-2)}\'")
-	stats.mem = runcmd("free -m | awk \'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }\'")
-	stats.disk_space = runcmd("df -h | awk \'$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}\'")
-	stats.ip = runcmd("hostname -I | cut -d\' \' -f1")
+	stats.cpu = str(runcmd("top -bn1 | grep load | awk \'{printf \"CPU Load: %.2f\", $(NF-2)}\'"))
+	stats.mem = str(runcmd("free -m | awk \'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }\'"))
+	stats.disk_space = str(runcmd("df -h | awk \'$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}\'"))
+	stats.ip = str(runcmd("hostname -I | cut -d\' \' -f1"))
 	return stats
 
 
