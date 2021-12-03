2eb6c8879d9bc7786d5275386e0813c781b6a7cc
jhawk7 <james.hawkins@careerbuilder.com>
2021-12-02 20:58:48 -0500

    updating stats test output pt2

--- stats_test.py
+++ stats_test.py
@@ -4,8 +4,8 @@ def generateStats():
 	#stats.temp = runcmd("vcgencmd measure_temp").split("temp=")[1]
 	temp = "n/a"
 	cpu = str(runcmd("top -bn1 | grep load | awk \'{printf \"%.2f\", $(NF-2)}\'"))
-	mem = str(runcmd("free -m | awk \'NR==2{printf \"%s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }\'"))
-	disk_space = str(runcmd("df -h | awk \'$NF==\"/\"{printf \"%d/%d GB  %s\", $3,$2,$5}\'"))
+	mem = str(runcmd("free -m | awk \'NR==2{printf \"%s/%s MB %.2f%%\", $3,$2,$3*100/$2 }\'"))
+	disk_space = str(runcmd("df -h | awk \'$NF==\"/\"{printf \"%d/%d GB %s\", $3,$2,$5}\'"))
 	ip = str(runcmd("hostname -I | cut -d\' \' -f1"))
 	print(f"Temp: {temp} \nCPU: {cpu} \nMEM: {mem} \nSpace: {disk_space} \nIP: {ip}")
 
