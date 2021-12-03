f1262aa1de73f92ef4dcc3ae02b118254fbd60e0
jhawk7 <james.hawkins@careerbuilder.com>
2021-12-02 20:56:57 -0500

    updating stats test output

--- stats_test.py
+++ stats_test.py
@@ -3,9 +3,9 @@ import subprocess
 def generateStats():
 	#stats.temp = runcmd("vcgencmd measure_temp").split("temp=")[1]
 	temp = "n/a"
-	cpu = str(runcmd("top -bn1 | grep load | awk \'{printf \"CPU Load: %.2f\", $(NF-2)}\'"))
-	mem = str(runcmd("free -m | awk \'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }\'"))
-	disk_space = str(runcmd("df -h | awk \'$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}\'"))
+	cpu = str(runcmd("top -bn1 | grep load | awk \'{printf \"%.2f\", $(NF-2)}\'"))
+	mem = str(runcmd("free -m | awk \'NR==2{printf \"%s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }\'"))
+	disk_space = str(runcmd("df -h | awk \'$NF==\"/\"{printf \"%d/%d GB  %s\", $3,$2,$5}\'"))
 	ip = str(runcmd("hostname -I | cut -d\' \' -f1"))
 	print(f"Temp: {temp} \nCPU: {cpu} \nMEM: {mem} \nSpace: {disk_space} \nIP: {ip}")
 
